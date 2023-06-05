7982d9eab83d3c96b8581aed7c85251ecc3518a7
Izzat <superizzat564@gmail.com>
2023-06-05 17:57:49 +0500

    Login component is styled

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -3,19 +3,18 @@ import './Logis.scss'
 import {AiOutlineEye, AiOutlineEyeInvisible} from 'react-icons/ai';
 const Login = () => {
   return (
-    <section className='login'>
-        <div className="--card">
-            <h2>Login</h2>
+    <section className='login --center-all'>
+        <div className="--card --bg-light">
+            <h2 className='--color-danger'>Login</h2>
             <div className="--form-control">
-                  <input type="text" placeholder='Username' />
+                  <input type="text" placeholder='Username' className='--width-100'/>
             <div className="password">
-                <input type="password" placeholder='Password'/>
+                <input type="password" placeholder='Password' className='--width-100'/>
                 <div className="icon">
                     <AiOutlineEye/>
-                    
                 </div>
-                <button>Login</button>
              </div>
+             <button className='--btn --btn-danger --btn-block'>Login</button>
             </div>
 
         </div>
